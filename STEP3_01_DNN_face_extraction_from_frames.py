f65c59063d96d8ae00eb142729be146c88b83c41
Duc Phan Doan <75497356+pierreduke@users.noreply.github.com>
2022-02-17 11:06:20 +0700

    Add files via upload

---
+++ STEP3_01_DNN_face_extraction_from_frames.py
@@ -0,0 +1,38 @@
+import time
+import cv2
+import glob
+import numpy as np
+import os
+
+start_time = time.time()
+
+net = cv2.dnn.readNetFromCaffe('face_detector_dnn/deploy.prototxt',
+                               'face_detector_dnn/res10_300x300_ssd_iter_140000.caffemodel')
+path = glob.glob("Wider_samples/*.jpg")
+saved = 0
+for file in path:
+    saved +=1
+    img = cv2.imread(file)
+    try:
+        (h, w) = img.shape[:2]
+    except Exception as e:
+        continue
+    blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    net.setInput(blob)
+    detections = net.forward()
+    if len(detections) > 0:
+        i = np.argmax(detections[0, 0, :, 2])
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.55:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            try:
+                face = img[startY:endY, startX:endX]
+                p = os.path.sep.join(['Wider_faces/dnn', "{}.jpg".format(saved)])
+                cv2.imwrite(p, face)
+                print("Đã lưu vào {}".format(p))
+            except Exception as e:
+                continue
+end_time = time.time()
+elapsed_time = end_time - start_time
+print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
